10e567ffd47d505ed4bae77b7b1793513fd4ca8d
Luiz Gustavo Mesquita <125809048+LuizGustavoM@users.noreply.github.com>
2025-06-20 16:11:17 -0300

    Add files via upload

--- Js/main.js
+++ Js/main.js
@@ -1,66 +1,5 @@
 const { text } = require("stream/consumers");
 
-// timeline em gsap
-let tl = gsap.timeline();
-
-// Efeito do menu hamburguer
-function effectMenu(){
-    const hamburguer = document.querySelector("#hamburguer");
-    const linha1 = document.querySelector(".linha1");
-    const linha2 = document.querySelector(".linha2");
-    const linha3 = document.querySelector(".linha3");
-    const menuLateral = document.querySelector("#menuLateral");
-    const aside = document.querySelector("#asideRedes");
-
-    if (getComputedStyle(linha2).display === "none"){
-
-        //efeito no hamburguer
-        linha1.style.transform = "rotate(0deg)"
-        linha1.style.top = "0px"
-
-        linha2.style.display = "inline"
-
-        linha3.style.transform = "rotate(0deg)"
-        linha3.style.bottom = "0px"
-
-        hamburguer.style.position = "relative"
-        hamburguer.style.right = "0rem"
-
-        // efeito do menu lateral
-        menuLateral.style.display = "none"
-
-        aside.style.display = "inline"
-
-        gsap.to("#menuBar", {
-            x:238
-        })
-
-    } else{
-
-        //efeito no hamburguer
-        linha1.style.transform = "rotate(44deg)"
-        linha1.style.top = "12px"
-
-        linha2.style.display = "none"
-
-        linha3.style.transform = "rotate(-45deg)"
-        linha3.style.bottom = "9px"
-
-        hamburguer.style.position = "fixed"
-        hamburguer.style.right = "0rem"
-
-        // efeito do menu lateral
-        menuLateral.style.display = "inline"
-
-        aside.style.display = "none"
-
-        gsap.to("#menuBar", {
-            x:-240,
-            duration:1
-        })
-    }
-};
-
 // link para sites externos
 function github(){
     window.open("https://www.github.com/LuizGustavoM");
@@ -76,11 +15,4 @@ function dev(){
 
 function freepik(){
     window.open("http://www.freepik.com")
-}
-
-//efeitos
-function acao1(){
-    gsap.to(".textButton", {
-        x:400,
-    });
 }
---
+++ Js/style.js
@@ -0,0 +1,66 @@
+// timeline em gsap
+let tl = gsap.timeline();
+
+// Efeito do menu hamburguer
+function effectMenu(){
+    const hamburguer = document.querySelector("#hamburguer");
+    const linha1 = document.querySelector(".linha1");
+    const linha2 = document.querySelector(".linha2");
+    const linha3 = document.querySelector(".linha3");
+    const menuLateral = document.querySelector("#menuLateral");
+    const aside = document.querySelector("#asideRedes");
+
+    if (getComputedStyle(linha2).display === "none"){
+
+        //efeito no hamburguer
+        linha1.style.transform = "rotate(0deg)"
+        linha1.style.top = "0px"
+
+        linha2.style.display = "inline"
+
+        linha3.style.transform = "rotate(0deg)"
+        linha3.style.bottom = "0px"
+
+        hamburguer.style.position = "relative"
+        hamburguer.style.right = "0rem"
+
+        // efeito do menu lateral
+        menuLateral.style.display = "none"
+        aside.style.display = "inline"
+
+        gsap.to("#menuBar", {
+            x:238
+        })
+
+        gsap.to("#contatos_menuLateral", {
+            x:238
+        })
+
+    } else{
+        //efeito no hamburguer
+        linha1.style.transform = "rotate(44deg)"
+        linha1.style.top = "12px"
+
+        linha2.style.display = "none"
+
+        linha3.style.transform = "rotate(-45deg)"
+        linha3.style.bottom = "9px"
+
+        hamburguer.style.position = "fixed"
+        hamburguer.style.right = "0rem"
+
+        // efeito do menu lateral
+        menuLateral.style.display = "inline"
+        aside.style.display = "none"
+
+        gsap.to("#menuBar", {
+            x:-240,
+            duration:.5
+        })
+
+        gsap.to("#contatos_menuLateral", {
+            x:-240,
+            duration:.5
+        })
+    }
+};
